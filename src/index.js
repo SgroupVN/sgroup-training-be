e57d4c1164440eb0a39b350baea1ab16ac5869ca
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2024-04-01 20:05:43 +0700

    basic routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,82 @@
 import express from 'express';
-
+import fs from 'fs';
+import bodyParser from 'body-parser';
 const app = express()
+
+app.use(bodyParser.json())
+
+// basic route 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+}
+)
+
+// read data with fs
+app.get('/data', (req, res) => {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+            res.send('Error reading file', err)
+            return
+        }
+        res.send(data)
+    })
+})
+
+app.post('/data', (req, res) => {
+    const { id , name } = req.body
+    const data = JSON.stringify({ id, name })
+    fs.writeFile('data.json', data, (err) => {
+        if (err) {
+            res.send('Error writing file', err)
+            return
+        }
+        res.send('Data saved')
+    })
+}
+)
+app.put('/data/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+            res.send('Error reading file', err)
+            return
+        }
+        const newData = JSON.parse(data).map(item => {
+            if (item.id === id) {
+                return { id, name }
+            }
+            return item
+        })
+        fs.writeFile('data.json', JSON.stringify(newData), (err) => {
+            if (err) {
+                res.send('Error writing file', err)
+                return
+            }
+            res.send('Data updated')
+        })
+    })
+}
+)
+app.delete('/data/:id', (req, res) => {
+    const { id } = req.params
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+            res.send('Error reading file', err)
+            return
+        }
+        const newData = JSON.parse(data).filter(item => item.id !== id)
+        fs.writeFile('data.json', JSON.stringify(newData), (err) => {
+            if (err) {
+                res.send('Error writing file', err)
+                return
+            }
+            res.send('Data deleted')
+        })
+    })
+}
+)
+
 
 
 
